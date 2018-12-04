dc51be83889459da7ada2cc042b279abcd064942
Entela Duka <enteladuka@gmail.com>
2018-12-04 18:38:47 -0500

    Created user model with tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
-class User < ApplicationRecord
+class User < ApplicationRecord #Inherits from ApplicationRecord/ApplicationRecord inherits from ActiveRecord::Base
+  before_save { self.email = email.downcase } #self is the current user
+
+  validates :username, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
+                       #validates the presence of username attribute ( .blank? -> generates error ["Name can't be blank"] )
+  VALID_EMAIL_REJEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true , length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REJEX },
+                    uniqueness: { case_sensitive: false } #rails infers uniquess should be true
+  validates :password, presence: true, length: { minimum: 8 }
+
+
+  # For uniqueness validation can also use   validates_uniqueness_of :username, :email ?
+
+  has_secure_password
 end
