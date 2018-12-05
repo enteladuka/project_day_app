4010f3a4edd22fa3457023cd7e9eb9535ea4981a
Entela Duka <enteladuka@gmail.com>
2018-12-04 20:58:09 -0500

    Added user signup and partial for errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,38 @@
 class UsersController < ApplicationController
-  def new
+  def index
   end
+
+  def show
+    @user = User.find(params[:id]) # how does params retrieve the user ID exactly?
+    #debugger use CTRL+D to exit debugger if needed
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/signup', to: 'users#new' #define route for signup page
+  post '/signup', to: 'users#create'#define route for signup submission
+
+  resources :users #index, show, new, edit, crete, update, destroy
 end
